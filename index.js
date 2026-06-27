1227dee30abbee374465f46f089c9be4d56581c1
ziad badry <badry8977@gmail.com>
2026-06-27 21:15:24 +0300

    added courses model and migrated to mongoDB instead of just a variable in memory

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 app.use(express.json());
+const url='mongodb+srv://ziadmohamed:012012012zZ!@learn-mongodb.u0tcvp0.mongodb.net/mongo_learning'
+const mongoose=require('mongoose')
+mongoose.connect(url).then(()=>{
+    console.log('mongo db connected')
+})
 const coursesRouter=require('./courses/courses.routes')
 app.use('/courses',coursesRouter)
 app.listen('5000', () => {
